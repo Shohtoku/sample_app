4290f30f0fc48a20c6f7477201b30d7163094fe4
shohtoku <syou0107@gmail.com>
2015-08-15 10:58:19 +0900

    Finish user registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
-Rails.application.routes.draw do
+SampleApp::Application.routes.draw do
+  devise_for :users, :controllers => {
+    :registrations => "registrations"
+  }
 
   resources :users, only: [:show]
-  devise_for :users
+
   root  'static_pages#home'
 
   match '/help',    to: 'static_pages#help',    via: 'get'
